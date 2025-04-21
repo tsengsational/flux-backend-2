ad4242e32ad9ca8d421c10044361619bfb494cf2
Jason Tseng <jason.f.tseng@gmail.com>
2025-04-21 13:26:15 -0400

    update wp-config creds

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -4,9 +4,9 @@
  */
 
 // ** Database settings - You can get this info from your web host ** //
-define('DB_NAME', 'flux-theatre');
-define('DB_USER', 'capdenise');
-define('DB_PASSWORD', 'spiders!');
+define('DB_NAME', 'wordpress');
+define('DB_USER', 'wordpress');
+define('DB_PASSWORD', 'wordpress');
 define('DB_HOST', 'db');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
