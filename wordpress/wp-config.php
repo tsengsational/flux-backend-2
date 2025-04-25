460df41a9d7dc3ca8778da2dc4e663c5f68fb7fe
Jason Tseng <jason.f.tseng@gmail.com>
2025-04-25 09:33:39 -0400

    fix configs

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -10,9 +10,6 @@ define('DB_PASSWORD', 'spiders_are_friends_4');
 define('DB_HOST', 'db');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true );
-define( 'WP_DEBUG_DISPLAY', true );
 
 /**#@+
  * Authentication unique keys and salts.
